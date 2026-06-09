e41f63f7904e7d76888c3608365a87e9221015e7
rebecacrestaniramos-dot <rebeca.crestani.ramos@escola.pr.gov.br>
2026-06-09 11:54:40 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,12 @@
+//0 Javacript é uma lingugem WEB
+
+//Variáveis - Valores armazenados na memória do computador
+
+//Funções - Blocos de código
+
+function carregar(){
+    //alert("Bem vindo ao java script")
+    var msg = window.document.getElementById('msg')
+
+    msg.innerHTML = `Desenvolvendo com DOM`
+}
